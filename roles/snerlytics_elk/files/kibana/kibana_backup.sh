c91d5b958b899a7acc03e924924b98134984c778
root <root@sner-dev>
2022-07-08 09:13:35 +0200

    snerlytics: drop roles and playbook from public release (will be refactored)

--- roles/snerlytics_elk/files/kibana/kibana_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-elasticdump \
-	--input=http://127.0.0.1:9200/.kibana \
-	--output=/usr/local/share/kibana-data.json \
-	--type=data \
-	--overwrite
--- roles/snerlytics_elk/files/kibana/kibana_restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-elasticdump \
-	--input=/usr/local/share/kibana-data.json \
-	--output=http://127.0.0.1:9200/.kibana \
-	--type=data
